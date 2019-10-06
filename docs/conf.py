b87f120eaeb241e8d0ae5fd4687e7ecb4edc67a2
h1nk <12993584+h1nk@users.noreply.github.com>
2019-10-06 03:54:20 -0400

    Add extlinks to Sphinx extensions & configure "valve-wiki" for extlinks

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,12 @@ project = 'aiomasterquery'
 copyright = '2019, insurgency.gg'
 author = 'insurgency.gg'
 extensions = [
+    'sphinx.ext.extlinks',
 ]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 html_static_path = ['_static']
+extlinks = {
+    'valve-wiki': ('https://developer.valvesoftware.com/wiki/%s', 'page'),
+}
